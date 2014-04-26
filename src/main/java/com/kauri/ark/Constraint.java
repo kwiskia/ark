04ba70c72cf1d139c7c708c48217474215822dea
Eric Fritz <eric@eric-fritz.com>
2014-04-25 22:56:12 -0500

    Update copyright.

--- src/main/java/com/kauri/ark/Constraint.java
+++ src/main/java/com/kauri/ark/Constraint.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of the ark package.
  *
- * Copyright (C) 2013, Eric Fritz
+ * Copyright (c) 2014 Eric Fritz
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
  * and associated documentation files (the "Software"), to deal in the Software without
